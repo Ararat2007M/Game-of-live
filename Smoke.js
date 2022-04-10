0df09baf903f2ae26f6063eb5edd9a148bd2dc3d
Ararat Mkrtchyan <araratmkrtchyan@MacBook-Pro-uzivatela-Samo.local>
2022-04-10 21:48:54 +0400

    prog3finaaal

--- Smoke.js
+++ Smoke.js
@@ -1,129 +1,129 @@
-// let LivingCreature = require('./LivingCreature')
+let LivingCreature = require('./LivingCreature')
 
-// module.exports = class Smoke extends LivingCreature {
-//     constructor(x, y) {
-//         super(x, y);
-//         this.energy = 20;
-//     }
+module.exports = class Smoke extends LivingCreature {
+    constructor(x, y) {
+        super(x, y);
+        this.energy = 20;
+    }
 
-//     getNewCoordinates() {
-//         this.directions = [
-//             [this.x - 1, this.y - 1],
-//             [this.x, this.y - 1],
-//             [this.x + 1, this.y - 1],
-//             [this.x - 1, this.y],
-//             [this.x + 1, this.y],
-//             [this.x - 1, this.y + 1],
-//             [this.x, this.y + 1],
-//             [this.x + 1, this.y + 1]
-//         ];
-//     }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
 
-//     chooseCell(character) {
-//         this.getNewCoordinates()
-//         return super.chooseCell(character);
-//     }
+    chooseCell(character) {
+        this.getNewCoordinates()
+        return super.chooseCell(character);
+    }
 
 
 
-//     move() {
+    move() {
         
-//         var emptyCells = this.chooseCell(0)
-//         var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        var emptyCells = this.chooseCell(0)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
         
-//         var emptyCells1 = this.chooseCell(1)
-//         var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)]
+        var emptyCells1 = this.chooseCell(1)
+        var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)]
 
-//         if (newCell) {
-//             var newX = newCell[0]
-//             var newY = newCell[1]
-//             matrix[newY][newX] = matrix[this.y][this.x]
-//             matrix[this.y][this.x] = 0
-//             this.x = newX
-//             this.y = newY
-//         } else if (newCell1) {
-//             var newX = newCell1[0]
-//             var newY = newCell1[1]
-//             matrix[newY][newX] = matrix[this.y][this.x]
-//             matrix[this.y][this.x] = 1
-//             this.x = newX
-//             this.y = newY
-//         }
-//     }
+        if (newCell) {
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        } else if (newCell1) {
+            var newX = newCell1[0]
+            var newY = newCell1[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 1
+            this.x = newX
+            this.y = newY
+        }
+    }
 
-//     eat() {
-//         // var emptyCells = this.chooseCell(2)
-//         // var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
-//         var emptyCells1 = this.chooseCell(3)
-//         var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)]
-//         var emptyCells2 = this.chooseCell(5)
-//         var newCell2 = emptyCells2[Math.floor(Math.random() * emptyCells2.length)]
-//         var emptyCells3 = this.chooseCell(6)
-//         var newCell3 = emptyCells3[Math.floor(Math.random() * emptyCells3.length)]
+    eat() {
+        // var emptyCells = this.chooseCell(2)
+        // var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+        var emptyCells1 = this.chooseCell(3)
+        var newCell1 = emptyCells1[Math.floor(Math.random() * emptyCells1.length)]
+        var emptyCells2 = this.chooseCell(5)
+        var newCell2 = emptyCells2[Math.floor(Math.random() * emptyCells2.length)]
+        var emptyCells3 = this.chooseCell(6)
+        var newCell3 = emptyCells3[Math.floor(Math.random() * emptyCells3.length)]
 
-//         // if (newCell) {
-//         //     var newX = newCell[0]
-//         //     var newY = newCell[1]
+        // if (newCell) {
+        //     var newX = newCell[0]
+        //     var newY = newCell[1]
 
-//         //     matrix[newY][newX] = matrix[this.y][this.x]
-//         //     matrix[this.y][this.x] = 0
-//         //     this.x = newX
-//         //     this.y = newY
-//         //     for (var i in grassEaterArr) {
-//         //         if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
-//         //             grassEaterArr.splice(i, 1)
-//         //             break
-//         //         }
-//         //     }
-//         // }
-//         // else 
-//         if (newCell1) {
-//             var newX = newCell1[0]
-//             var newY = newCell1[1]
+        //     matrix[newY][newX] = matrix[this.y][this.x]
+        //     matrix[this.y][this.x] = 0
+        //     this.x = newX
+        //     this.y = newY
+        //     for (var i in grassEaterArr) {
+        //         if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+        //             grassEaterArr.splice(i, 1)
+        //             break
+        //         }
+        //     }
+        // }
+        // else 
+        if (newCell1) {
+            var newX = newCell1[0]
+            var newY = newCell1[1]
 
-//             matrix[newY][newX] = matrix[this.y][this.x]
-//             matrix[this.y][this.x] = 0
-//             this.x = newX
-//             this.y = newY
-//             for (var i in predatorArr) {
-//                 if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
-//                     predatorArr.splice(i, 1)
-//                     break
-//                 }
-//             }
-//         }
-//         else if (newCell2) {
-//             var newX = newCell2[0]
-//             var newY = newCell2[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in predatorArr) {
+                if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                    predatorArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else if (newCell2) {
+            var newX = newCell2[0]
+            var newY = newCell2[1]
 
-//             matrix[newY][newX] = matrix[this.y][this.x]
-//             matrix[this.y][this.x] = 0
-//             this.x = newX
-//             this.y = newY
-//             for (var i in humanArr) {
-//                 if (newX == humanArr[i].x && newY == humanArr[i].y) {
-//                     humanArr.splice(i, 1)
-//                     break
-//                 }
-//             }
-//         }
-//         else if (newCell3) {
-//             var newX = newCell3[0]
-//             var newY = newCell3[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in humanArr) {
+                if (newX == humanArr[i].x && newY == humanArr[i].y) {
+                    humanArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else if (newCell3) {
+            var newX = newCell3[0]
+            var newY = newCell3[1]
 
-//             matrix[newY][newX] = matrix[this.y][this.x]
-//             matrix[this.y][this.x] = 0
-//             this.x = newX
-//             this.y = newY
-//             for (var i in eagleArr) {
-//                 if (newX == eagleArr[i].x && newY == eagleArr[i].y) {
-//                     eagleArr.splice(i, 1)
-//                     break
-//                 }
-//             }
-//         }
-//         else {
-//             this.move()
-//         }
-//     }
-// }
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in eagleArr) {
+                if (newX == eagleArr[i].x && newY == eagleArr[i].y) {
+                    eagleArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+}
